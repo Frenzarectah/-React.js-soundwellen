c14a022c29f6d81a1d7927e86a05143d8e79eb7f
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-09-20 15:28:17 +0200

    adding a new logo and a keyframe transition

---
+++ src/script.js
@@ -0,0 +1,8 @@
+
+/*window.onload=()=>{ 
+    
+    setTimeout(() =>{
+        var logo = document.getElementById("soundwellen");
+        logo.className += "logo_appear";        
+    }, 3000);
+}*/
