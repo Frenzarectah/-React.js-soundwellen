1e78f56dff79260f9e03c058d01f7e83aafbc06a
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-09-20 16:51:41 +0200

    added a subtitle component + new font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './imgs/logo_novect.png';
 import './App.css';
 import './script.js';
+import Subtitle from './Subtitle';
 function App() {
   return (
     <div className="App">
         <img src={logo} className="logo" alt="logo" id="soundwellen" />
-
+        <Subtitle />
     </div>
   );
 }
---
+++ src/Subtitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+function Subtitle() {
+    return (
+        <div className="subtitle">coming soon...</div>
+    );
+  }
+
+export default Subtitle;
