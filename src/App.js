254e18a7e605fe4745e9b5cd40c687af51ab6879
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-09-23 17:15:38 +0200

    added a menu icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './imgs/logo_nowrite.jpg';
 import './App.css';
-//import funct from './script.js';
 import Subtitle from './Subtitle';
-import { Component } from 'react';
+import icon from './imgs/menu_ico.jpg';
 import Navbar from './Navbar';
 function App(){
   function changePage(){
     var logo = document.getElementById("soundwellen");
     logo.className="slideAway";
   }
+  function fadIn(){
+    var icon = document.querySelector(".icon");
+    icon.className+="_fadeIn";
+  }
   return (
     <div className="App">
+          <img src={icon} className="icon" onClick={fadIn}/>
           <div className="head"><Navbar menu1="home" menu2="contatti" menu3="djs"/></div>
           <a href="#" onClick={changePage} className="sound">
             <img src={logo} className="logo" alt="logo" id="soundwellen" />
