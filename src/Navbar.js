f0e55d260ab6fc178fc16d299e825f1564fd2761
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-10-01 12:29:11 +0200

    changed menu style in responsive mode

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,14 +4,10 @@ import frame from './imgs/frame.jpg';
 
 var openDJS =()=>{
  alert("massivo");
- var freim = document.getElementById("freim");
- freim.style.opacity="1";
- freim.className+="frame_in";
 }
 function Navbar(props){
     return(
         <react.Fragment>
-        <img src={frame} id="freim" className="frame"/>
         <div id="list"><ul>
             <li onClick={()=>openDJS()}>{props.menu1}</li>
             <li>{props.menu2}</li>
