0621c1140c91eab711bb9468e524ae0936d5755d
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-10-12 19:58:29 +0200

    added a _redir file to allow netlify deploying

--- src/artists.js
+++ src/artists.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Navi from './Navbar.js';
 var Artists = () =>{
-
     return(
         <React.Fragment>
-        <h1 style={{color:"white"}}>ARTISTIIIIIIII</h1>
+        <h1 style={{color:"white"}}>ARTISTI</h1>
         </React.Fragment>
 );
 }
