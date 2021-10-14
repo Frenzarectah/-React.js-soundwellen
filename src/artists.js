5aff2ac37f8d1b7893b3724d826f451ced7d86ac
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-10-14 19:08:42 +0200

    added some style to artists page

--- src/artists.js
+++ src/artists.js
@@ -1,12 +1,13 @@
 import React from 'react';
 var Artists = () =>{
-    var head = document.getElementsByClassName("nav-link");
-    for (var el of head){
-      el.classList+="menu_appear";
+    window.onload=()=>{
+        var head = document.getElementsByClassName("nav-link");
+        for (var el of head){
+            el.classList.add("menu_appear");
+        }
     }
     return(
         <React.Fragment>
-        
         <h1 style={{color:"white"}}>ARTISTI</h1>
         </React.Fragment>
 );
