4d59fe444e43b95b8f738c618e1fb6074c4c58b6
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-10-15 09:24:18 +0200

    changed artists page

--- src/artists.js
+++ src/artists.js
@@ -1,11 +1,10 @@
 import React from 'react';
 var Artists = () =>{
-    window.onload=()=>{
-        var head = document.getElementsByClassName("nav-link");
-        for (var el of head){
-            el.classList.add("menu_appear");
-        }
+    var head = document.getElementsByClassName("nav-link");
+    for (var el of head){
+      el.classList+="menu_appear";
     }
+    
     return(
         <React.Fragment>
         <h1 style={{color:"white"}}>ARTISTI</h1>
