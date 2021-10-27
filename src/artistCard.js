d284e137f9ac0dc237ab003fe3cb31d6376ec36b
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-10-27 20:55:09 +0200

    added a momentary img for testing

--- src/artistCard.js
+++ src/artistCard.js
@@ -5,7 +5,7 @@ var ArtistCard = (props)=>{
     return(
         <div className="frame">
             <div>[{props.name}]</div>
-            <div><img style={{width:"75px",height:"auto"}} src={props.pic} alt="artist photo"/></div>
+            <div className="photo"><img src={props.pic} alt="artist photo"/></div>
             <div style={{fontFamily:'Open Sans Condensed'}}>{props.bio}</div>
         </div>
 );}
--- src/artists.js
+++ src/artists.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ArtistCard from './artistCard';
-import pic1 from './imgs/vevol.jpg';
+import pic1 from './imgs/not-av.png';
 function changePage(){
   var logo = document.getElementById("soundwellen");
   var sub = document.getElementsByClassName("subtitle")[0];
