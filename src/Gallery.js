0239d5122e19dab9c8fb13fe174891902313b9ae
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-12-02 17:24:09 +0100

    added img as gallery navigation

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ArtistCard from "./artistCard";
 import './Gallery.css';
 import pic1 from './imgs/not-av.png';
+import arrow from './imgs/back.png';
+import arrow_fwd from './imgs/forward.png';
 var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 var Gallery = (props)=>{
@@ -22,13 +24,13 @@ var Gallery = (props)=>{
     return(
     <div className="gal">
         <div className="sx" style={{fontFamily:"arial"}} onClick={()=>prevClick()}>
-                    {'<<'}
+                   <img src={arrow}/>
         </div>
         <div className="image">
                     {items[indx]}
         </div>
         <div className="dx" style={{fontFamily:"arial"}} onClick={()=>nextClick()}>
-                    {'>>'}
+                    <img src={arrow_fwd}/>
         </div>
     </div>
 
